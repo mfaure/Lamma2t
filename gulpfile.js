24f973e0d04692c80e8d81004420dc9dd46a6d2f
Matthieu FAURE <mfaure@asqatasun.org>
2017-07-10 18:01:26 +0200

    code reformat

--- gulpfile.js
+++ gulpfile.js
@@ -15,54 +15,54 @@ var minifyCSS = require('gulp-minify-css');
 // JS hint task
 gulp.task('jshint', function () {
     gulp.src('./src/JS/*.js')
-            .pipe(jshint())
-            .pipe(jshint.reporter('default'));
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 // minify new images
 gulp.task('imagemin', function () {
     var imgSrc = './src/Images/**/*',
-            imgDst = './build/Images';
+        imgDst = './build/Images';
 
     gulp.src(imgSrc)
-            .pipe(changed(imgDst))
-            .pipe(imagemin())
-            .pipe(gulp.dest(imgDst));
+        .pipe(changed(imgDst))
+        .pipe(imagemin())
+        .pipe(gulp.dest(imgDst));
 });
 
 // minify new or changed HTML pages
 gulp.task('htmlpage', function () {
     var htmlSrc = './src/*.html',
-            htmlDst = './build';
+        htmlDst = './build';
 
     gulp.src(htmlSrc)
-            .pipe(changed(htmlDst))
-            .pipe(minifyHTML())
-            .pipe(gulp.dest(htmlDst));
+        .pipe(changed(htmlDst))
+        .pipe(minifyHTML())
+        .pipe(gulp.dest(htmlDst));
 });
 
 // JS concat, strip debugging and minify
 gulp.task('scripts', function () {
     gulp.src(['./src/JS/lib.js', './src/JS/*.js'])
-            .pipe(concat('Script.js'))
-            .pipe(stripDebug())
-            .pipe(uglify())
-            .pipe(gulp.dest('./build/JS/'));
+        .pipe(concat('Script.js'))
+        .pipe(stripDebug())
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/JS/'));
 });
 
 // CSS concat, auto-prefix and minify
 gulp.task('styles', function () {
     gulp.src(['./src/CSS/*.css'])
-            .pipe(concat('Styles.css'))
-            .pipe(autoprefix('last 2 versions'))
-            .pipe(minifyCSS())
-            .pipe(gulp.dest('./build/CSS/'));
+        .pipe(concat('Styles.css'))
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./build/CSS/'));
 });
 
 // Copy PHP files
 gulp.task('phpcopy', function () {
     gulp.src(['./src/index.php'])
-            .pipe(gulp.dest('./build/'));
+        .pipe(gulp.dest('./build/'));
 });
 
 // default gulp task
--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-        const MAX_FORECAST = "57"; //count begins from 1 (not 0) to MAX_FORECAST
+const MAX_FORECAST = "57"; //count begins from 1 (not 0) to MAX_FORECAST
 
 $tzUTC = new DateTimeZone('UTC');
 $tzParis = new DateTimeZone('Europe/Paris');
@@ -7,10 +7,11 @@ $tzParis = new DateTimeZone('Europe/Paris');
 $myTimeStamp = new DateTime("now", $tzParis);
 $myTimeStamp->setTimezone($tzUTC);
 
-$modelInitDate = computeModelInitDate();
+$modelInitDate = computeInitDate();
 $forecasts = array();
 
-class Forecast {
+class Forecast
+{
 
     // Images optimised, with relative (local) URL
     const WIND_URL_OPTIMISED_STUB = "images_2optimised/wind10m_N_web_";
@@ -31,7 +32,8 @@ class Forecast {
      * - the number of the forecast (describing the time shift from the init)
      */
 
-    function computeModelValidDate($index) {
+    function computeValidDate($index)
+    {
         global $modelInitDate;
         $timeShiftInHours = NULL;
         $modelValidDate = clone $modelInitDate;
@@ -42,24 +44,25 @@ class Forecast {
         } elseif ($index <= self::STEPS_OF_3HOURS_FORECASTS) {
             // 37 < index <= 49 => shift = 37 hours + (index-37) * 3 hours - 1
             $timeShiftInHours = self::STEPS_OF_1HOUR_FORECASTS +
-                    ($index - self::STEPS_OF_1HOUR_FORECASTS) * 3 - 1;
+                ($index - self::STEPS_OF_1HOUR_FORECASTS) * 3 - 1;
         } else {
             //  49 < index => shift = 37 hours + 12 * 3 hours + (index-49)*6 hours - 1
             $timeShiftInHours = self::STEPS_OF_1HOUR_FORECASTS +
-                    (self::STEPS_OF_3HOURS_FORECASTS - self::STEPS_OF_1HOUR_FORECASTS) * 3 +
-                    ($index - self::STEPS_OF_3HOURS_FORECASTS) * 6 - 1;
+                (self::STEPS_OF_3HOURS_FORECASTS - self::STEPS_OF_1HOUR_FORECASTS) * 3 +
+                ($index - self::STEPS_OF_3HOURS_FORECASTS) * 6 - 1;
         }
 
         $myTimeShift = DateInterval::createFromDateString($timeShiftInHours . ' hours');
         return $modelValidDate->add($myTimeShift);
     }
 
-    function __construct($i) {
+    function __construct($i)
+    {
         // Construct the URL of the image of the forecast
         $this->ImageUrlOptimised = self::WIND_URL_OPTIMISED_STUB . $i . self::WIND_URL_OPTIMISED_EXT;
 
         // Construct the timestamp of the forecast ("validDate")
-        $this->validDate = $this->computeModelValidDate($i);
+        $this->validDate = $this->computeValidDate($i);
     }
 
 }
@@ -69,8 +72,8 @@ class Forecast {
  * the current hour
  */
 
-function computeModelInitDate() {
-    $myModelInitDate = "NULL";
+function computeInitDate()
+{
     global $tzUTC;
     global $tzParis;
     global $myTimeStamp;
@@ -90,7 +93,8 @@ function computeModelInitDate() {
     if ($myTimeStamp <= $ModelRun01Morning) {
         // before 7h30 UTC: init = day - 1 at 12h UTC
         $myModelInitDate = new DateTime("now", $tzUTC);
-        $myModelInitDate = $myModelInitDate->sub(new DateInterval('P1D')); // P1D = Period 1 day cf http://php.net/manual/fr/dateinterval.construct.php
+        // P1D = Period 1 day cf http://php.net/manual/fr/dateinterval.construct.php
+        $myModelInitDate = $myModelInitDate->sub(new DateInterval('P1D'));
         $myModelInitDate = $myModelInitDate->setTime(12, 00);
     } elseif ($myTimeStamp >= $ModelRun02Evening) {
         //  after 21h30 UTC: init = day at 12h UTC
@@ -104,7 +108,8 @@ function computeModelInitDate() {
     return $myModelInitDate;
 }
 
-function displayForecasts() {
+function displayForecasts()
+{
     global $myTimeStamp;
     global $forecasts;
     global $modelInitDate;
@@ -135,7 +140,8 @@ function displayForecasts() {
  * Construct the array of all 57 forecasts
  */
 
-function initLamma2T() {
+function initLamma2T()
+{
     global $forecasts;
 
     for ($i = 1; $i <= MAX_FORECAST; $i++) {
@@ -143,49 +149,49 @@ function initLamma2T() {
     }
 }
 
-# initialisation
+/* initialisation */
 initLamma2T();
 ?>
 
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <title>Lamma2T : Prévisions Météo Bouches de Bonifacio / Maddalena de LammaRete adapté par Matthieu 2T</title>
-        <meta charset="UTF-8" >
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" media="all" href="CSS/Styles.css">
-    </head>
-    <body>
+<head>
+    <title>Lamma2T : Prévisions Météo Bouches de Bonifacio / Maddalena de LammaRete adapté par Matthieu 2T</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="all" href="CSS/Styles.css">
+</head>
+<body>
 
-        <div id="c" class="carousel">
-            <?php displayForecasts(); ?>
-        </div>
+<div id="c" class="carousel">
+    <?php displayForecasts(); ?>
+</div>
 
-        <div id="about">
-            <a href="about.html">À propos de Lamma2T</a>
-        </div>
+<div id="about">
+    <a href="about.html">À propos de Lamma2T</a>
+</div>
 
-        <footer>
-            Matthieu 2T - matthieu CHEZ stramanari.eu
-        </footer>
+<footer>
+    Matthieu 2T - matthieu CHEZ stramanari.eu
+</footer>
 
-        <script src="JS/Script.js"></script>
-        <script type="text/javascript">
-            var _paq = _paq || [];
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function () {
-                var u = "//MyPiwikServer";
-                _paq.push(['setTrackerUrl', u + 'piwik.php']);
-                _paq.push(['setSiteId', MyPiwikSiteId]);
-                var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-                g.type = 'text/javascript';
-                g.async = true;
-                g.defer = true;
-                g.src = u + 'piwik.js';
-                s.parentNode.insertBefore(g, s);
-            })();
-        </script>
-        <noscript><p><img src="//MyPiwikCompleteServerSiteId" style="border:0;" alt="" /></p></noscript>
-    </body>
+<script src="JS/Script.js"></script>
+<script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function () {
+        var u = "//MyPiwikServer";
+        _paq.push(['setTrackerUrl', u + 'piwik.php']);
+        _paq.push(['setSiteId', MyPiwikSiteId]);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.type = 'text/javascript';
+        g.async = true;
+        g.defer = true;
+        g.src = u + 'piwik.js';
+        s.parentNode.insertBefore(g, s);
+    })();
+</script>
+<noscript><p><img src="//MyPiwikCompleteServerSiteId" style="border:0;" alt=""/></p></noscript>
+</body>
 </html>
